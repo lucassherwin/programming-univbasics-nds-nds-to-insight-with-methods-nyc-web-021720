364ae9e42a35beda6d4ff5caf4ea51f264eaded2
Lucas Sherwin <lksherwin@gmail.com>
2020-01-28 19:26:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,7 +7,7 @@ def gross_for_director(director_data)
   index = 0
   total = 0
   while index < director_data.length do
-    total += director_data[i][:movies]
+    total += director_data[index][:movies]
   end
   return total
 end
